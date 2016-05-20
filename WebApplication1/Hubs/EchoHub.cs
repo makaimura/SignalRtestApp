f885d6388b6c92cdde637914ea07e85e1ccfc1d7
4za9W8C8 <4za9W8C8@M001C44N4>
2016-05-20 13:38:05 +0900

    プロジェクト ファイルを追加します。

---
+++ WebApplication1/Hubs/EchoHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace WebApplication1.Hubs
+{
+    [HubName("Echo")]
+    public class EchoHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Send(string text)
+        {
+            Clients.All.Receive(text);
+        }
+    }
+}
